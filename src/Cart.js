e70d79163468afb9d66314978300b985c0faf9b4
parente-formations <107269755+parente-formations@users.noreply.github.com>
2022-07-26 15:20:10 +0100

    Initial commit

---
+++ src/Cart.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Cart = () => {
+  return <Title>Your cart total is 300.00$</Title>;
+};
+
+export default Cart;
+
+const Title = styled.p`
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 20px;
+`;
